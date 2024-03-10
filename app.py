4b53cde4dae47382bec42a93e299f50c48c60ce7
subinbh <subin.bhatta@gmail.com>
2024-03-10 23:40:08 +0545

    Importing modules , loading env variables

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print("This is for test the env")
+import streamlit as st
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+load_dotenv() # Loading all the environment variable
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
